944653164014176f1e4d12f1a842f6d9d2024025
jsoctocat <42134925+jsoctocat@users.noreply.github.com>
2025-03-28 23:58:29 -0700

    Relearning react for github page pt 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <Provider>
       <App />
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 );
